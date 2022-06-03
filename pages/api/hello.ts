ff04976a570974c040144c9aa81da75bf279773d
ht1204 <ptb1204@hotmail.com>
2022-06-03 13:25:58 -0500

    app from scratch-first commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+type StringOrNumber = string | number;
+
 type Data = {
-  name: string
+  name: string;
+  age: StringOrNumber;
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Doe', age: 12 })
 }
